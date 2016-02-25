690b534f78bb5e44565d5e2ad1093b5177b4bee7
Sadainer Hernandez <sadainer@hotmail.com>
2016-02-25 18:07:05 -0500

    Validacion gps activado

--- app/src/main/java/com/example/admin_sena/miambulacia/MainActivity.java
+++ app/src/main/java/com/example/admin_sena/miambulacia/MainActivity.java
@@ -1,12 +1,18 @@
 package com.example.admin_sena.miambulacia;
 
 import android.Manifest;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationManager;
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ProgressBar;
 
@@ -27,14 +33,38 @@ public class MainActivity extends AppCompatActivity {
 
         locManager = (LocationManager) getSystemService(LOCATION_SERVICE);
 
+        if (!locManager.isProviderEnabled(LocationManager.GPS_PROVIDER))
+        {
 
-       timer.schedule(new TimerTask() {
-           @Override
-           public void run() {
-               Intent IntEmergencia = new Intent(MainActivity.this, MapActivity_Pedido.class);
-               startActivity(IntEmergencia);
-               MainActivity.this.finish();
-           }
-       }, 2000);
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle("GPS Desactivado") //
+                    .setMessage("Necesita activar GPS para mejorar su ubicación") //
+                    .setPositiveButton("Aceptar", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            // TODO
+                            Intent myIntent = new Intent( Settings.ACTION_LOCATION_SOURCE_SETTINGS );
+                            startActivity(myIntent);
+                        }
+                    }) //
+                    .setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            // TODO
+                            dialog.dismiss();
+                            MainActivity.this.finish();
+                        }
+                    });
+            builder.show();
+//
+        }
+//           timer.schedule(new TimerTask() {
+//           @Override
+//           public void run() {
+//               Intent IntEmergencia = new Intent(MainActivity.this, MapActivity_Pedido.class);
+//               startActivity(IntEmergencia);
+//               MainActivity.this.finish();
+//           }
+//       }, 2000);
     }
+
+
 }
