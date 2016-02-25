c6d0fee131b2f8ae085e344bc1f361170eb0fc17
OscarV2 <ing.odvega@gmail.com>
2016-02-24 23:10:35 -0500

    commit MapActivity_Pedido Java

--- app/src/main/java/com/example/admin_sena/miambulacia/MapsActivity_Seguimiento.java
+++ app/src/main/java/com/example/admin_sena/miambulacia/MapsActivity_Seguimiento.java
@@ -1,7 +1,10 @@
 package com.example.admin_sena.miambulacia;
 
+import android.content.Intent;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -22,6 +25,18 @@ public class MapsActivity_Seguimiento extends FragmentActivity implements OnMapR
 //        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
   //              .findFragmentById(R.id.map);
     //    mapFragment.getMapAsync(this);
+
+        Button btnCancelarPedido = (Button)findViewById(R.id.btnCancelarPedido);
+        btnCancelarPedido.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent =
+                        new Intent(MapsActivity_Seguimiento.this ,CalificacionServicioActivity.class);
+                startActivity(intent);
+
+            }
+        });
+
     }
 
 
