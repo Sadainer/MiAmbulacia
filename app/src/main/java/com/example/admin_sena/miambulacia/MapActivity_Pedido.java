377cf2de77357387696ea475eb9ca74a790bf29e
OscarV2 <ing.odvega@gmail.com>
2016-02-24 21:55:35 -0500

    commit MapActivity_Pedido Java

--- app/src/main/java/com/example/admin_sena/miambulacia/MapActivity_Pedido.java
+++ app/src/main/java/com/example/admin_sena/miambulacia/MapActivity_Pedido.java
@@ -1,18 +1,33 @@
 package com.example.admin_sena.miambulacia;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapActivity_Pedido extends FragmentActivity implements OnMapReadyCallback {
+public class MapActivity_Pedido extends AppCompatActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    public LocationManager locationManager;
+    private LocationListener locationListener;
+    private Location loc;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,19 +36,24 @@ public class MapActivity_Pedido extends FragmentActivity implements OnMapReadyCa
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);  //
+        mMap.setMapType(GoogleMap.MAP_TYPE_TERRAIN);        //declarar tipo de mapa
         mapFragment.getMapAsync(this);
+
+        Button btnEnviarAlerta = (Button)findViewById(R.id.butPedirAmbulancia);
+    btnEnviarAlerta.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+
+            Intent intent =
+                    new Intent(MapActivity_Pedido.this,MapsActivity_Seguimiento.class);
+                    startActivity(intent);
+        }
+    });
+
     }
 
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
@@ -42,5 +62,6 @@ public class MapActivity_Pedido extends FragmentActivity implements OnMapReadyCa
         LatLng sydney = new LatLng(-34, 151);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Mi Ubicación"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
     }
 }
